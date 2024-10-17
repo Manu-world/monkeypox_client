0ee0978c15b53b86a9fbb65bc39274011246a25c
Manu-world <manu.softwareengineer@gmail.com>
2024-10-17 14:42:30 +0000

    fix: package conflict

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,8 +1,8 @@
 // src/api/auth.js
 import axios from "axios";
 
-const API_URL = "https://monkeypox-api.onrender.com/api/auth/";
-// const API_URL = "http://localhost:5000/api/auth/";
+// const API_URL = "https://monkeypox-api.onrender.com/api/auth/";
+const API_URL = "http://localhost:5000/api/auth/";
 
 axios.defaults.withCredentials = true;
 
